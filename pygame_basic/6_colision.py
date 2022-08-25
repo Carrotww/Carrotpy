c2c1d3c3e4d5015a16391062443d980135dd595e
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-08-25 13:35:54 +0900

    Update 6_colision.py

--- pygame_basic/6_colision.py
+++ pygame_basic/6_colision.py
@@ -31,6 +31,14 @@ to_y = 0
 # 이동 속도
 character_speed = 0.6
 
+# 적 캐릭터
+enemy = pygame.image.load("C:\\Users\\유형석\\PycharmProjects\\pythonProject\\Carrotpy\\pygame_basic\\enemy.png")
+enemy_size = enemy.get_rect().size # 이미지의 크기를 구해 올 수 있음
+enemy_width = enemy_size[0]
+enemy_height = enemy_size[1]
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2) # 화면 가로의 절반 크기에 해당하는 곳에 위치함
+enemy_y_pos = screen_height / 2 - enemy_height / 2
+
 # Event loop
 running = True # 게임 진행 여부 확인
 
@@ -74,10 +82,25 @@ while running:
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+    # 충돌 처리를 위한 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌 했습니다")
+        running = False
+
     # screen.fill((0, 0, 255)) # 이런 식으로 RGB를 주어서 배경이 가능
     screen.blit(background, (0, 0)) # 배경 그리기 왼쪽 위 끝에서 부터 사진을 채우는 것
 
-    screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # 적 그리기
 
     pygame.display.update() # pygame은 게임 화면을 계속 그려주어야 함 이게 없으면 배경이 보이지 않음
 
